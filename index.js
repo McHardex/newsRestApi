e606acc90c248c23d17a4b27f58281c18864548e
mchardex <mchardex@gmail.com>
2018-11-27 15:17:39 +0100

    reversed port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
 module.exports = server;
