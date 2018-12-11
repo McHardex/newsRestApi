0629b9c40f1959947a673f7d4178adb54278d235
mchardex <mchardex@gmail.com>
2018-12-12 00:11:42 +0100

    fixed CORS issue

--- startup/routes.js
+++ startup/routes.js
@@ -7,16 +7,16 @@ const forgotPassword = require('../routes/forgotPassword');
 const resetPassword = require('../routes/resetPassword');
 const updatePasswordViaEmail = require('../routes/updatePasswordViaEmail');
 const error = require('../middleware/error');
+const cors = require('cors');
 
 module.exports = function(app) {
+  app.use(express.json());
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Headers", "Origin, x-auth-token, Content-Type, Accept");
     next();
   });
-  app.use(express.json());
   app.use('/api/articles', articles);
   app.use('/api/writers', writers);
   app.use('/api/users', users);
