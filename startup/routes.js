df2586bd1f2f342167785e8a6d622cc56f41a86d
mchardex <mchardex@gmail.com>
2018-12-11 22:44:29 +0100

    fixing CORS issue

--- startup/routes.js
+++ startup/routes.js
@@ -14,6 +14,7 @@ module.exports = function(app) {
   app.use(express.json());
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Headers", "Origin, x-auth-token, Content-Type, Accept");
     next();
   });
